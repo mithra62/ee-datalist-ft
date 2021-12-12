29d632a2a340efa641ad087a89bcc0e1c1b1de82
Eric Lamb <eric@mithra62.com>
2021-12-11 21:44:00 -0700

    completes coverage of addon.setup file

--- system/user/addons/datalist/src/Tests/AddonSetupTest.php
+++ system/user/addons/datalist/src/Tests/AddonSetupTest.php
@@ -52,12 +52,72 @@ class AddonSetupTest extends TestCase
     /**
      * @depends testAuthorValue
      * @param $addon
-     * @return Provider1
+     * @return Provider
      */
     public function testSettingsValue($addon): Provider
     {
-        $addon = ee('App')->get('datalist');
         $this->assertFalse($addon->get('settings_exist'));
+        return $addon;
+    }
+
+    /**
+     * @depends testSettingsValue
+     * @param $addon
+     * @return Provider
+     */
+    public function testFieldtypesIndexExists($addon): Provider
+    {
+        $this->assertIsArray($addon->get('fieldtypes'));
+        return $addon;
+    }
+
+    /**
+     * @depends testFieldtypesIndexExists
+     * @param $addon
+     * @return Provider
+     */
+    public function testFieldNameIsProper($addon): Provider
+    {
+        $field_data = $addon->get('fieldtypes');
+        $this->assertTrue(!empty($field_data['datalist']['name']));
+        $this->assertEquals('DataList', $field_data['datalist']['name']);
+        return $addon;
+    }
+
+    /**
+     * @depends testFieldNameIsProper
+     * @param $addon
+     * @return Provider
+     */
+    public function testFieldCompatibility($addon): Provider
+    {
+        $field_data = $addon->get('fieldtypes');
+        $this->assertTrue(!empty($field_data['datalist']['compatibility']));
+        $this->assertEquals('list', $field_data['datalist']['compatibility']);
+        return $addon;
+    }
+
+    /**
+     * @depends testFieldCompatibility
+     * @param $addon
+     * @return Provider
+     */
+    public function testSeederIndexExists($addon): Provider
+    {
+        $this->assertIsArray($addon->get('seeder'));
+        return $addon;
+    }
+
+    /**
+     * @depends testSeederIndexExists
+     * @param $addon
+     * @return Provider
+     */
+    public function testSeederField($addon): Provider
+    {
+        $seeder = $addon->get('seeder');
+        $this->assertTrue(!empty($seeder['fields']['datalist']));
+        $this->assertEquals('Mithra62\DataList\Fields\DataList', $seeder['fields']['datalist']);
         return $addon;
     }
 }
