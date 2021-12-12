cb4314d89f54e8bd45412bf6e430d2e625222cc7
Eric Lamb <eric@mithra62.com>
2021-12-11 17:48:10 -0700

    initial file import

---
+++ system/user/addons/datalist/addon.setup.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'author'            => 'mithra62',
+    'author_url'        => '',
+    'name'              => 'DataList',
+    'description'       => 'Allows for a datalist FieldType ',
+    'version'           => '0.0.1',
+    'namespace'         => 'Mithra62\DataList',
+    'settings_exist'    => false,
+    'fieldtypes'        => [
+        'datalistft' => [
+            'name' => 'DataList',
+            'compatibility' => '',
+        ],
+    ],
+    // Advanced settings
+];
